f1c8b3cd124ba6c8bd8b657334f6cd0f17c6d277
Yusuke Ohashi <github@junkpiano.me>
2018-09-23 15:33:57 +0900

    Print link

--- lib/freebook.rb
+++ lib/freebook.rb
@@ -2,18 +2,20 @@
 require 'selenium-webdriver'
 
 class Freebook
-    def info
-        driver = Selenium::WebDriver.for :safari
-        driver.manage.window.maximize
-        driver.navigate.to "https://www.packtpub.com/packt/offers/free-learning"
+  def info
+    driver = Selenium::WebDriver.for :safari
+    driver.manage.window.maximize
+    driver.navigate.to "https://www.packtpub.com/packt/offers/free-learning"
 
-        element = driver.find_element(class: 'dotd-title')
-        driver.action.move_to(element)
-        puts element.text.strip
+    element = driver.find_element(class: 'dotd-title')
+    driver.action.move_to(element)
+    puts element.text.strip
 
-        element = driver.find_element(class: 'packt-js-countdown')
-        driver.action.move_to(element)
-        puts element.text.strip
-        driver.quit
-    end
+    element = driver.find_element(class: 'packt-js-countdown')
+    driver.action.move_to(element)
+    puts element.text.strip
+    driver.quit
+
+    puts 'go get? -> https://www.packtpub.com/packt/offers/free-learning'
+  end
 end
